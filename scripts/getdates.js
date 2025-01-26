2a0fc252756825ebd09626afb328463658ef8258
ita-faith <eti21002@byui.edu>
2025-01-26 00:40:07 -1100

    jibbs

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Dynamically update the copyright year
+const yearSpan = document.getElementById("year");
+const lastModified = document.getElementById("lastModified");
+
+yearSpan.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
